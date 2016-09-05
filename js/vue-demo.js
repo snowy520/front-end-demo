3f8469c029e30be9919fd1217adba1d85de50f5b
xueliming <liming.snowy@gmail.com>
2016-09-05 21:56:11 +0800

    添加一个vue demo

--- js/vue-demo.js
+++ js/vue-demo.js
@@ -4,6 +4,27 @@
 new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue.js!'
+    message: 'Hello Vue.js!',
+    newTodo: '',
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue.js' },
+      { text: 'Build Something Awesome' }
+    ]
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    },
+    addTodo: function () {
+      var text = this.newTodo.trim();
+      if (text) {
+        this.todos.push({ text: text });
+        this.newTodo = ''
+      }
+    },
+    removeTodo: function (index) {
+      this.todos.splice(index, 1)
+    }
   }
 });
